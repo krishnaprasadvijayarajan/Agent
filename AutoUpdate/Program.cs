a1475cef179f1dcd58f3070fdc4452e3e7e3ea48
Krishna Prasad Vijayarajan <krishnaprasadvijayarajan@users.noreply.github.com>
2020-11-01 10:43:02 +0000

    Add project files.

---
+++ AutoUpdate/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agent
+{
+    class Program
+    {
+        static string version = "0.1";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am agent {0}", version);
+            Console.ReadLine();
+        }
+    }
+}
